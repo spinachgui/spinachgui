b82277921fe1d73ba22a8855edee01166e13aac2
gareth <gareth@fbaf8dc8-a59a-9f45-831a-ff29167b0329>
2009-08-10 15:43:27 +0000

    Added some eigenvalue tests which show the eigenvalues obtained are wrong.
    git-svn-id: https://gorion.oerc.ox.ac.uk:8443/svn/spinach_gui@135 fbaf8dc8-a59a-9f45-831a-ff29167b0329

--- wxSpinach/py/driver.py
+++ wxSpinach/py/driver.py
@@ -10,3 +10,27 @@ diag=spinsys.Matrix3(1,0,0,
                      0,0,3)
 v=diag.getEigenvectorX()
 print v
+
+print """Expected Eigenvalues 1,2,3"""
+
+diag=spinsys.Matrix3(1,2,3,
+                     4,5,6,
+                     7,8,9)
+v=diag.getEigenvectorX()
+print v
+print """Expected Eigenvalues 16.1169,-1.1168,0.000"""
+
+diag=spinsys.Matrix3(1,0,1,
+                     2,2,3,
+                     5,7,2)
+v=diag.getEigenvectorX()
+print v
+print """Expected Eigenvalues 7.2386,0.6268,-2.8654"""
+
+
+diag=spinsys.Matrix3(1,0,-6,
+                     0,1,0,
+                     6,0,2)
+v=diag.getEigenvectorX()
+print v
+print """Expected Eigenvalues 1.5 + 5.9791j,1.5-5.9791j,1.0000"""

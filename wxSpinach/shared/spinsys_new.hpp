d53f660d347d0c79f227a81d9df74b608e773d06
gareth <gareth@fbaf8dc8-a59a-9f45-831a-ff29167b0329>
2009-08-17 14:41:12 +0000

    Plan implimentation of a much, much cleaner interface to the spin system which keeps the XML parsing code at arm's length. Should be fast enough that there shouldn't be a problem using it directly in numerical code
    git-svn-id: https://gorion.oerc.ox.ac.uk:8443/svn/spinach_gui@154 fbaf8dc8-a59a-9f45-831a-ff29167b0329

---
+++ wxSpinach/shared/spinsys_new.hpp
@@ -0,0 +1,214 @@
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+const long END=-1;
+
+class SpinSystem;
+class Spin;
+class Orientation;
+class Interaction;
+class ReferenceFrame;
+
+class SpinSystemElement {};
+
+class Vector3 {
+    Vector3();
+    Vector3(double x,double y,double z);
+
+    double GetX();
+    double GetY();
+    double GetZ();    
+  private:
+    double x;
+    double y;
+    double z;
+};
+
+class Matrix3 {
+  public:
+    Matrix3();
+    Matrix3(double a00,double a01,double a02,
+            double a10,double a11,double a12,
+            double a20,double a21,double a22);
+    
+    const double* GetRaw();
+    
+    double Get(long i, long j);
+    double operator() (long i,long j);
+    void Set(long i,long j,double val);
+  private:
+    double raw[9];
+};
+
+class SpinSystem {
+  public:
+    SpinSystem();
+    ~SpinSystem();
+    
+    long GetSpinCount();    
+    Spin* GetSpin(long n);
+    vector<Spin*> GetSpins();
+    void InsertSpin(Spin* _Spin,long Position=END);
+    void RemoveSpin(long Position);
+    void RemoveSpin(Spin* _Spin);
+
+    ReferenceFrame* GetRootFrame();
+    void SetRootFrame(ReferenceFrame* Frame);
+    
+    void LoadFromGamesFile(const char* filename);
+    void LoadFromG03File(const char* filename);
+    void LoadFromXMLFile(const char* filename);
+  private:
+    friend class Spin;
+    
+        
+};
+
+class Spin {
+  public:
+    Spin(SpinSystem* Parent);
+    ~Spin();
+
+    Vector3 GetPosition();
+    void SetPosition(Vector3 Position);
+
+    void SetLabel(string Label);
+    const char* GetLabel();
+    
+    long GetInteractionCount();
+    Interaction* GetInteraction(long n);
+    vector<Interaction*> GetInteractions();
+    void InsertInteraction(Interaction* _Interaction,long Position=END);
+    void RemoveInteraction(long Position);
+    void RemoveInteraction(Interaction* _Interaction);
+    
+    ReferenceFrame* GetFrame();
+    void SetFrame(ReferenceFrame* Frame);
+  private:
+    SpinSystem* Parent;
+};
+
+class Orientation {
+  public:
+    Orientation();
+    ~Orientation();
+    
+    enum Type {
+        EULER,
+        ANGLE_AXIS,
+        QUATERNION,
+        EIGENSYSTEM
+    };
+    
+    Type GetType();
+    Matrix3 GetAsMatrix3();
+
+    void GetEuler(double* alpha,double* beta,double* gamma);
+    void GetAngleAxis(double* angle,Vector3* axis);
+    void GetQuaternion(double* real, double* i, double* j, double* k);
+    void GetEigenSystem(Vector3* XAxis,Vector3* YAxis, Vector3* ZAxis);
+
+    void SetEuler(double alpha,double beta,double gamma);
+    void SetAngleAxis(double angle,Vector3 axis);
+    void SetQuaternion(double real, double i, double j, double k);
+    void SetEigenSystem(Vector3* XAxis,Vector3* YAxis, Vector3* ZAxis);
+  private:
+    union  {
+        struct {
+            double alpha;
+            double beta;
+            double gamma;
+        } mEuler;
+        struct {
+            double angle;
+            Vector3* axis;
+        } mAngleAxis;
+        struct {
+            double real;
+            double i;
+            double j;
+            double k;
+        } mQuaternion;
+        struct {
+            Vector3* XAxis;
+            Vector3* YAxis;
+            Vector3* ZAxis;
+        } mEigenSystem;
+    } mData;
+    Type mType;
+};
+
+class Interaction {
+  public:
+    Interaction();
+    ~Interaction();
+    
+    enum Type {
+        SCALAR,
+        MATRIX,
+        EIGENVALUES,
+        AXRHOM,
+        SPANSKEW
+    };
+    
+    Type GetType();
+    Matrix3 GetAsMatrix3();
+    
+    void GetScalar(double* Scalar);
+    void GetMatrix(Matrix3* Matrix);
+    void GetEigenvalues(double* XX,double* YY, double* ZZ, Orientation* Orient);
+    void GetAxRhom(double* iso,double* ax, double* rh, Orientation* Orient);
+    void GetSpanSkew(double* iso,double* Span, double* Skew, Orientation* Orient);
+
+    void SetScalar(double Scalar);
+    void SetMatrix(Matrix3* Matrix);
+    void SetEigenvalues(double XX,double YY, double ZZ, Orientation* Orient);
+    void SetAxRhom(double iso,double ax, double rh, Orientation* Orient);
+    void SetSpanSkew(double iso,double Span, double Skew, Orientation* Orient);
+  private:
+    union  {
+      double mScalar;
+      Matrix3* mMatrix;
+      struct {
+        double XX;
+        double YY;
+        double ZZ;
+        Orientation* Orient;
+      } mEigenvalues;
+      struct {
+          double iso;
+          double ax;
+          double rh;
+          Orientation* Orient;
+      } mAxRhom;
+      struct {
+          double iso;
+          double span;
+          double skew;
+          Orientation* Orient;
+      } mSpanSkew;
+   } mData;
+   Type mType;
+};
+
+class ReferenceFrame {
+public:
+    ReferenceFrame();
+    ~ReferenceFrame();
+    
+    long GetChildCount();
+    ReferenceFrame* GetChildFrame(long n);
+    vector<ReferenceFrame*> GetChildFrames();
+    void InsertChild(ReferenceFrame* Frame,long Position=END);
+    void RemoveChild(long Position);
+    void RemoveChild(ReferenceFrame* Child);
+    
+    Vector3 GetPosition();
+    Orientation GetOrientation();
+};
+
+
+

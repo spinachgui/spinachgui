1479ed492f1f3412d4b1d405b6b7eddb3549db71
gareth <gareth@boo.localdomain>
2009-10-07 03:32:29 +0100

    Created an isotope configureation file

--- wxSpinach/shared/nuclear_data.hpp
+++ wxSpinach/shared/nuclear_data.hpp
@@ -16,13 +16,24 @@ double getElementB(long p);
 
 //Isotope functions
 
-long getIsotopeCount();
-long getIsotope(long protonN,long massN);
-long getIsotopeBySymbol(const char* str);
-double getIsotopeColorR(long index);
-double getIsotopeColorG(long index);
-double getIsotopeColorB(long index);
-const char* getIsotopeSymbol(long index);
+///Call this somewhere in the initalisation to load the istope data
+///from data/isotopes.dat
+void LoadIsotopes();
+
+///Get the number of known isotopes of a given element
+long getIsotopeCount(long protonN);
+///Get the index'th isotope of a given element where 0 <= index <
+///getIsotopeCount()
+
+//
+long getNeutronCount(long protonN,long index);
+
+///
+long getMultiplicity(long protonN,long index);
+
+///
+double getGamma(long protonN,long index);
+
 
 
 #endif // __spinachcalcFrameBase__

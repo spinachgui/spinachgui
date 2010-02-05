2f028d27c2686282580c5e8c3401712b4e8bf3e2
Gareth Charnock <gareth@Jaheira.(none)>
2010-02-05 20:36:13 +0000

    Add some real code to logSlider. LogSlider alows the selection of a value on an infinate log scale by dragging. Still needs signaling so it can be hooked up to the interactions though.

--- src/gui/LogSlider.cpp
+++ src/gui/LogSlider.cpp
@@ -1,30 +1,104 @@
 
 #include <gui/LogSlider.hpp>
 #include <wx/dcclient.h>
+#include <iostream>
+#include <cmath>
+#include <stdexcept>
 
-LogSlider::LogSlider(wxWindow* parent) : wxControl(parent,wxID_ANY) {
-  
+using namespace std;
+
+LogSlider::LogSlider(wxWindow* parent,double value)
+    : wxControl(parent,wxID_ANY) {
+    if(value<=0) {
+	throw std::runtime_error("In LogSlider value<=0");
+    }
+    mLogValue=log10(value);
+    mLogWidth=4;
+    mLogValueDelta=0;
+}
+
+void LogSlider::SetValue(double value) {
+    if(value<=0) {
+	throw std::runtime_error("In LogSlider value<=0");
+    }
+    mLogValue=value;
+    wxClientDC dc(this);
+    RealPaint(dc);
+}
+
+double LogSlider::GetValue() {
+    return pow(10.0,mLogValue);
 }
 
 void LogSlider::OnPaint(wxPaintEvent& e) {
-  wxPaintDC dc(this);
-  dc.DrawLine(0,0,5,5);
+    wxPaintDC dc(this);
+    RealPaint(dc);
+}
+
+void LogSlider::RealPaint(wxDC& dc) {
+    wxSize size=GetClientSize();
+    int w=size.GetWidth();
+    int h=size.GetHeight();
+
+    dc.DrawRectangle(0,0,w,h);
+
+    dc.DrawLine(0,h/2,w,h/2);
+
+    wxPoint triangle[3];
+    triangle[0]=wxPoint(0,6);
+    triangle[1]=wxPoint(3,-2);
+    triangle[2]=wxPoint(-3,-2);
+    dc.DrawPolygon(3,triangle,w/2,4);
+
+    double logMin=mLogValue+mLogValueDelta-mLogWidth/2;
+    double logMax=mLogValue+mLogValueDelta+mLogWidth/2;
+    
+    int pixelsPer10=w/mLogWidth;
+
+    int pixel02=pixelsPer10*log10(2);
+    int pixel04=pixelsPer10*log10(4);
+    int pixel06=pixelsPer10*log10(6);
+    int pixel08=pixelsPer10*log10(8);
+
+    for(double line=floor(logMin);line<ceil(logMax);line++) {
+	int pixel=pixelsPer10 * (line-logMin);
+
+	dc.DrawText(wxString() << pow(10,line),pixel,h/2-8);
+	dc.DrawLine(pixel,h/2+10,pixel,h/2-10);
+
+	dc.DrawLine(pixel+pixel02,h/2+4,pixel+pixel02,h/2-4);
+	dc.DrawLine(pixel+pixel04,h/2+4,pixel+pixel04,h/2-4);
+	dc.DrawLine(pixel+pixel06,h/2+4,pixel+pixel06,h/2-4);
+	dc.DrawLine(pixel+pixel08,h/2+4,pixel+pixel08,h/2-4);
+    }
+}
+
+void LogSlider::OnLeftDown(wxMouseEvent&e) {
+    mStartDrag=e.GetX();
+    mLogValueDelta=0;
+    Refresh();
 }
 
 void LogSlider::OnLeftUp(wxMouseEvent&e) {
-  if(e.Dragging()) {
-    
-  }
+    mLogValue+=mLogValueDelta;
+    mLogValueDelta=0;
+    mStartDrag=0;
+    Refresh();
 }
 
 void LogSlider::OnMouseMove(wxMouseEvent&e) {
-  if(e.Dragging()) {
-    
-  }
+    if(e.Dragging()) {
+	wxSize size=GetClientSize();
+	int w=size.GetWidth();
+
+	mLogValueDelta=double(mStartDrag-e.GetX())/double(w) * mLogWidth;
+    }
+    Refresh();
 }
 
 BEGIN_EVENT_TABLE(LogSlider, wxControl)
 EVT_LEFT_UP(LogSlider::OnLeftUp)
+EVT_LEFT_DOWN(LogSlider::OnLeftDown)
 EVT_MOTION(LogSlider::OnMouseMove)
 EVT_PAINT(LogSlider::OnPaint)
 END_EVENT_TABLE()
--- src/gui/LogSlider.hpp
+++ src/gui/LogSlider.hpp
@@ -7,15 +7,26 @@
 
 class LogSlider : public wxControl {
 public:
-  LogSlider(wxWindow* parent);
-  void OnPaint(wxPaintEvent& e);
+    LogSlider(wxWindow* parent, double value);
+    void OnPaint(wxPaintEvent& e);
 
-  void OnLeftUp(wxMouseEvent&e);
-  void OnMouseMove(wxMouseEvent&e);
+    void OnLeftUp(wxMouseEvent&e);
+    void OnLeftDown(wxMouseEvent&e);
+    void OnMouseMove(wxMouseEvent&e);
 
-  sigc::signal<void,double> sigChange;
+    void SetValue(double value);
+    double GetValue();
+
+    sigc::signal<void,double> sigChange;
 protected:
-  DECLARE_EVENT_TABLE()
+    DECLARE_EVENT_TABLE()
+    void RealPaint(wxDC& dc);
+    double mLogValue;
+    double mLogValueDelta;
+    //The pixel that is being hovered over
+    int mStartDrag;
+    ///The Number of orders of magnitude to show
+    double mLogWidth; 
 private:
 };
 

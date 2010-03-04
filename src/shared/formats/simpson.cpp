51bcb44652183560a0507606adc55c18e8545009
Gareth Charnock <gareth@Jaheira.(none)>
2010-03-04 15:18:06 +0000

    Progress on the SIMPSON output filter.

--- src/shared/formats/simpson.cpp
+++ src/shared/formats/simpson.cpp
@@ -2,22 +2,23 @@
 #include <shared/formats/simpson.hpp>
 #include <shared/nuclear_data.hpp>
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
 using namespace SpinXML;
 
 const char* example_par_section =
-    "par {"
-    "  proton_frequency 400e6"
-    "  spin_rate 10000"
-    "  sw spin_rate/2.0"
-    "  np 32"
-    "  crystal_file rep320"
-    "  gamma_angles 18"
-    "  start_operator I1x"
-    "  detect_operator I1p"
-    "  verbose 1101"
-    "  variable rf 150000"
+    "par {\n"
+    "  proton_frequency 400e6\n"
+    "  spin_rate 10000\n"
+    "  sw spin_rate/2.0\n"
+    "  np 32\n"
+    "  crystal_file rep320\n"
+    "  gamma_angles 18\n"
+    "  start_operator I1x\n"
+    "  detect_operator I1p\n"
+    "  verbose 1101\n"
+    "  variable rf 150000\n"
     "}";
 
 
@@ -26,19 +27,63 @@ void SIMPSONLoader::SaveFile(const SpinSystem* ss,const char* filename) const {
   if(!fout.is_open()) {
     throw runtime_error("Couldn't open file");
   }
+  vector<Spin*> spins=ss->GetSpins();
+  long count=spins.size();
 
-  long count=ss->GetSpinCount();
   fout << "spinsys {" << endl;
-  fout << "  channels 13C 1H" << endl;
-  fout << "  nuclei";
-  for(long i=0;i<count;i++) {
-    Spin* spin=ss->GetSpins()[i];
+  fout << "  channels ";
+  vector<pair<long,long> > chanels;
+  for(long i=0;i<count;i++){
+    Spin* spin=spins[i];
     long element=spin->GetElement();
-    long isotope=spin->GetIsotopes()[0];
-    fout << isotope << getElementSymbol(element)  << " ";
+    long neutronN=spin->GetIsotopes()[0];
+
+    if(getNuclearSpin(element,neutronN) ==0) {
+        //We don't have an NMR active isotope, skip it
+        continue;
+    }
+    pair<long,long> p(element,neutronN);
+    if(find(chanels.begin(),chanels.end(),p)==chanels.end()) {
+        chanels.push_back(p);
+    }
+  }
+  for(unsigned long i=0;i<chanels.size();i++) {
+      pair<long,long> p = chanels[i];
+      fout << p.second+p.first << getElementSymbol(p.first)  << " ";
   }
   fout << endl;
-  fout << " dipole" << endl;
+  fout << "  nuclei ";
+  for(long i=0;i<count;i++) {
+    Spin* spin=spins[i];
+    long element=spin->GetElement();
+    long isotope=spin->GetIsotopes()[0]+element;
+    if(getNuclearSpin(element,isotope-element) ==0) {
+        //We don't have an NMR active isotope, skip it
+        continue;
+    }
+    fout << isotope << getElementSymbol(element)  << " ";
+  }
   fout << " shift" << endl;
+  for(long i=0;i<count;i++) {
+
+  }
+
+  for(long i=0;i<count;i++) {
+      //Obtain the dipole-dipole interactions
+      Spin* spin=spins[i];
+      vector<Interaction*> inters=spin->GetInteractionsOnce();
+      
+      for(vector<Interaction*>::iterator it=inters.begin();
+          it != inters.end();++it) {
+          if((*it)->GetSubType() == Interaction::ST_DIPOLAR) {
+              fout << "  dipole " 
+                   << i << " " 
+                   << ss->GetSpinNumber((*it)->GetOtherSpin(spin))
+                   << " "<< endl;
+          }
+      }
+  }
   fout << "}" << endl;
+  
+  fout << example_par_section << endl;
 }

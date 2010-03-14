dee2dbefa6075a1ca9f0a7bb498c96704b805ea1
Gareth Charnock <gareth.charnock@gmail.com>
2010-03-14 21:59:58 +0000

    Start defining all the dimensions in the SI unit system

--- src/shared/unit.hpp
+++ src/shared/unit.hpp
@@ -22,14 +22,53 @@ unit type being assigned to the ddouble)
 *************************************************************/
 
 
-#define unit_template_dec1 int E1,int L1
-#define unit_template_dec2 int E2,int L2
+#define unit_template_dec1 int L1,int M1,int t1,int C1, int T1, int LU1, int MOL1
+#define unit_template_dec2 int L2,int M2,int t2,int C2, int T2, int LU2, int MOL2
 
-#define unit_template_inst1 E1,L1
-#define unit_template_inst2 E2,L2
+#define unit_template_inst1  L1, M1, t1, C1,  T1,  LU1,  MOL1
+#define unit_template_inst2  L2, M2, t2, C2,  T2,  LU2,  MOL2
 
-#define unit_template_combine(utc_op) E1 utc_op E2,L1 utc_op L2
-#define unit_template_inst_with_metafunc1(metafuc) E1 metafuc,L1 metafuc
+#define _concan(L1, M1, t1, C1,  T1,  LU1, MOL1,  L2, M2, t2, C2,  T2,  LU2,  MOL2) \
+    L1+L2, M1+M2, t1+t2, C1+C2,  T1+T2,  LU1+LU2, MOL1+MOL2
+#define _c(a,b)      _concan(a,b)
+
+
+#define _concan3(L1, M1, t1, C1,  T1,  LU1, MOL1,  L2, M2, t2, C2,  T2,  LU2,  MOL2,  L3, M3, t3, C3,  T3,  LU3,  MOL3) \
+    L1+L2+L3, M1+M2+M3, t1+t2+t3, C1+C2+C3,  T1+T2+T3,  LU1+LU2+LU3, MOL1+MOL2+MOL3
+#define _c3(a,b,c)      _concan3(a,b,c)
+
+//Define the basic units
+#define _zero             0,0,0,0,0,0,0
+#define _length(x)        x,0,0,0,0,0,0
+#define _mass(x)          0,x,0,0,0,0,0
+#define _time(x)          0,0,x,0,0,0,0
+#define _current(x)       0,0,0,x,0,0,0
+#define _temp(x)          0,0,0,0,x,0,0
+#define _lum_intensity(x) 0,0,0,0,0,x,0  //Probably will never be needed
+#define _mole(x)          0,0,0,0,0,0,x  
+
+//Now define the derived units
+#define _energy(x)    _c3(_mass(x),_length(2*x),_time(-2*x))
+#define _power(x)     _c3(_mass(x),_length(2*x),_time(-3*x))
+#define _force(x)     _c3(_mass(x),_length(x),  _time(-2*x))
+#define _frequency(x) _time(-x)
+
+#define unit_template_combine(utc_op)					\
+    L1 utc_op L2,							\
+	M1   utc_op M1  ,						\
+	t1   utc_op t1  ,						\
+	C1   utc_op C1  ,						\
+	T1   utc_op T1  ,						\
+	LU1  utc_op LU1 ,						\
+	MOL1 utc_op MOL1							
+#define unit_template_inst_with_metafunc1(metafuc)			\
+    L1   metafuc,							\
+	M1   metafuc,							\
+	t1   metafuc,							\
+	C1   metafuc,							\
+	T1   metafuc,							\
+	LU1  metafuc,							\
+	MOL1 metafuc							
 
 template<unit_template_dec1>
 class unit {
@@ -126,8 +165,8 @@ struct ddouble {
 ///Special case of the unitless ddouble, which can be convered to and
 ///from a regular double implicityly
 template<>
-struct ddouble<0,0> {
-    typedef ddouble<0,0> self_t;
+struct ddouble<_zero> {
+    typedef ddouble<_zero> self_t;
     ///An uninitalised ddouble works just like an uninitalised double
     ddouble() {
     }
@@ -140,7 +179,7 @@ struct ddouble<0,0> {
     ddouble(double _si) : si(_si) {
     }
     ///Construct a 
-    ddouble(double quantity,const unit<0,0>& u) 
+    ddouble(double quantity,const unit<_zero>& u) 
         : si(u.toSI(quantity)){
     }
     //static ddouble_helper<0,0> helper;
@@ -148,8 +187,8 @@ struct ddouble<0,0> {
     ///gap=energy_gap[MHz]; Not thread safe, I'm not sure how to
     ///achive thread safeness right now, and it probably doesn't
     ///matter.
-    const ddouble_helper<0,0> operator[](const unit<0,0>& u) {
-        return ddouble_helper<0,0>(&si,u);
+    const ddouble_helper<_zero> operator[](const unit<_zero>& u) {
+        return ddouble_helper<_zero>(&si,u);
     }
     //The physical quatity in SI units
     double si;
@@ -236,18 +275,24 @@ ddouble<unit_template_inst1> operator-(ddouble<unit_template_inst1> with_unit) {
     return ddouble<unit_template_inst1>(-with_unit);
 }
 
+typedef ddouble<_length(1)> length;
+typedef ddouble<_length(2)> length2;
+typedef ddouble<_length(3)> length3;
+typedef ddouble<_length(4)> length4;
+typedef ddouble<_length(5)> length5;
 
+typedef ddouble<_mass(1)> mass;
+typedef ddouble<_time(1)> time_d;
+typedef ddouble<_current(1)> current;
+typedef ddouble<_temp(1)> temperature;
+typedef ddouble<_mole(1)> mole;
 
-typedef ddouble<1,0> energy;
-typedef ddouble<0,1> length;
-typedef ddouble<0,2> length2;
-typedef ddouble<0,3> length3;
-typedef ddouble<0,4> length4;
-typedef ddouble<0,5> length5;
+typedef ddouble<_energy(1)> energy;
+typedef ddouble<_energy(2)> energy2;
 
-typedef unit<1,0> energy_unit;
-typedef unit<0,1> length_unit;
-typedef unit<0,0> no_unit;
+typedef unit<_energy(1)> energy_unit;
+typedef unit<_length(1)> length_unit;
+typedef unit<_zero> no_unit;
 
 const no_unit     unitless("Unitless",1.0);
 

97897e281b246e6c7bd1001815c1419f0d3a04b5
Gareth Charnock <gareth@Jaheira.(none)>
2010-03-19 14:14:02 +0000

    Removed the old openGL drawing classes.

--- src/gui/MolSceneGraph.hpp
+++ src/gui/MolSceneGraph.hpp
@@ -6,53 +6,6 @@
 #include <gui/Display3D.hpp>
 
 
-///Scenegraph node that draws a spin
-class SpinNode : public SGNode {
-public:
-    SpinNode(Spin* spin);
-    void OnSpinDying(Spin*) {delete this;} //Arguments are usused
-
-    void OnSpinHover(Spin* spin);
-    void OnSpinSelect(std::vector<SpinXML::Spin*> spins);
-private:
-
-    Spin* mSpin;
-    virtual void RawDraw(SpinachDC& dc);
-    virtual void ToPovRay(wxString& src);
-
-    bool mHover;
-    bool mSelected;
-};
-
-class InterNode : public SGNode {
-public:
-    InterNode(SpinXML::Spin* spin, SpinXML::Interaction::SubType st);
-    void OnSpinDying(Spin*) {delete this;}
-    void LoadInteractionMatrix();
-
-    void OnNewInteraction(Interaction* inter);
-private:
-    SpinXML::Spin* mSpin;
-    SpinXML::Interaction::SubType mType;
-
-    void SetMatrix(const Matrix3& mat);
-    virtual void RawDraw(SpinachDC& dc);
-    virtual void ToPovRay(wxString& src);
-    float mat[16];
-};
-
-
-class MoleculeNode : public SGNode {
-public:
-    MoleculeNode(SpinSystem* ss);
-    void OnReload();
-    void OnNewSpin(SpinXML::Spin* newSpin,long number);
-
-private:
-    virtual void RawDraw(SpinachDC& dc);
-    SpinSystem* mSS;
-    virtual void ToPovRay(wxString& src);
-};
 
 class MoleculeFG : public SGNode {
 public:

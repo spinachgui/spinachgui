3935af84c869764d4fa8d5c31566611277d61e89
Gareth Charnock <gareth@Jaheira.(none)>
2010-03-30 20:08:35 +0100

    Implimented rotation conversions. Code still needs debugging as nan often seems to show up.

--- src/shared/mathtypes.cpp
+++ src/shared/mathtypes.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 using namespace SpinXML;
 
+#define PI 3.141592653589793238462643
 
 //============================================================//
 // Constants
@@ -15,6 +16,110 @@ using namespace SpinXML;
 const double SpinXML::hbar=6.626068e-34;
 const double SpinXML::bohr_mag=9.27400915e-24;
 const double SpinXML::mu0=1.25663706e-6;
+
+//============================================================//
+// Simple Euler Angles Type
+
+
+struct EulerAngles {
+    double alpha,beta,gamma;
+    EulerAngles(double _alpha,double _beta, double _gamma) 
+        : alpha(_alpha),beta(_beta),gamma(_gamma) {
+
+    }
+};
+
+//============================================================//
+// Simple Quaternion Type
+
+
+struct Quaternion {
+    double w,x,y,z;
+    Quaternion(){}
+    Quaternion(double _w,double _x, double _y, double _z) 
+        : w(_w),x(_x),y(_y),z(_z){
+    }
+    Quaternion(double angle,const Vector3& axis) {
+        SetAngleAxis(angle,axis);
+    }
+    Quaternion operator*(const Quaternion& q2) {
+	Quaternion res;
+	double w1=w; double w2=q2.w;
+	double x1=x; double x2=q2.x;
+	double y1=y; double y2=q2.y;
+	double z1=z; double z2=q2.z;
+	res.w=w1*w2 - x1*x2 - y1*y2 - z1*z2;
+	res.x=w1*x2 + x1*w2 + y1*z2 - z1*y2;
+	res.y=w1*y2 - x1*z2 + y1*w2 + z1*x2;
+	res.z=w1*z2 + x1*y2 - y1*x2 + z1*w2;	
+
+	return res;
+    }
+    Vector3 Transform(Vector3 v) {
+        Quaternion v_as_q;
+        v_as_q.w=0;
+        v_as_q.x=v.GetX();
+        v_as_q.y=v.GetY();
+        v_as_q.z=v.GetZ();
+
+        v_as_q=(*this)*v_as_q*Quaternion(w,-x,-y,-z);
+
+        Vector3 ret;
+        ret.SetX(v_as_q.x);
+        ret.SetY(v_as_q.y);
+        ret.SetZ(v_as_q.z);
+        return ret;
+    }
+    void SetAngleAxis(double a,const Vector3& v) {
+        double _x=v.GetX();
+        double _y=v.GetY();
+        double _z=v.GetZ();
+
+	double norm = sqrt(_x*_x+_y*_y+_z*_z);
+	_x/=norm;
+	_y/=norm;
+	_z/=norm;
+
+	double sin_a=sin(a/2);
+	w=cos(a/2);
+	x=_x*sin_a;
+	y=_y*sin_a;
+	z=_z*sin_a;
+    }
+    double norm() {
+	return sqrt(w*w+x*x+y*y+z*z);
+    }
+    Quaternion Conjugulate() {
+        return Quaternion(w,-x,-y,-z);
+    }
+    EulerAngles ToEuler() {
+        Vector3 z_axis=Vector3(0,0,1);
+        Vector3 x_axis=Vector3(1,0,0);
+        z_axis=Transform(z_axis);
+        x_axis=Transform(x_axis);
+    
+        double gamma=atan2(z_axis.GetY(),z_axis.GetX());
+        double beta=atan2(sqrt(z_axis.GetX()*z_axis.GetX() + z_axis.GetY()*z_axis.GetY()).si,z_axis.GetZ().si);
+
+        //Use γ and β to rotate V2 back onto the point 0,0,1
+        Quaternion gammaTwist,betaTwist;
+        betaTwist.SetAngleAxis(-beta,Vector3(0,1,0));
+        gammaTwist.SetAngleAxis(-gamma,Vector3(0,0,1));
+
+        x_axis=(betaTwist*gammaTwist).Transform(x_axis);
+
+        double alpha = atan2(x_axis.GetY(),x_axis.GetX());
+        if(alpha < 0 || alpha >= 2*PI)  alpha = alpha-2*PI*floor(alpha/(2*PI));
+        if(alpha >= 2*PI) alpha = 0;
+        if(beta < 0 || beta >= PI)    beta = beta-  PI*floor(beta/PI);
+        if(gamma < 0 || gamma >= 2*PI)  gamma = gamma-2*PI*floor(gamma/(2*PI));
+        if(gamma >= 2*PI) gamma = 0;
+	return EulerAngles(alpha,beta,gamma);
+    }
+
+};
+
+
 
 
 //==============================================================================//
@@ -173,6 +278,7 @@ Orientation::Orientation() : mType(UNDEFINED) {
 Orientation::Orientation(const Orientation& orient) :
   mType(orient.mType) {
   if(mType==ANGLE_AXIS) {
+    mData.mAngleAxis.angle = mData.mAngleAxis.angle;
     mData.mAngleAxis.axis = new Vector3(*orient.mData.mAngleAxis.axis);
   } else if(mType==EIGENSYSTEM) {
     mData.mEigenSystem.XAxis = new Vector3(*orient.mData.mEigenSystem.XAxis);
@@ -338,7 +444,7 @@ Matrix3 Orientation::GetAsMatrix() const {
     double a11 = cos_alpha*cos_gamma - cos_beta * sin_gamma * sin_alpha;
     double a22 =-sin_alpha*sin_gamma + cos_beta * cos_gamma * cos_alpha;
     double a33 = cos_beta;
-    
+
     double a12 = cos_alpha*sin_gamma + cos_beta * cos_gamma * sin_alpha;
     double a21 =-sin_alpha*cos_gamma + cos_beta * sin_gamma * sin_alpha;
     
@@ -454,5 +560,197 @@ string Orientation::ToString() const {
 
 void Orientation::FromString(std::string string) {
   cerr << "Not Implimented" << endl;
+}
+
+
+
+Orientation Orientation::GetAsEuler() const {
+    switch(mType) {
+    case EULER: {
+        return Orientation(*this);
+    }
+    case ANGLE_AXIS: {
+        Orientation q=GetAsQuaternion();
+        return q.GetAsEuler();
+    }
+    case QUATERNION: {
+        Quaternion q(mData.mQuaternion.real,
+                     mData.mQuaternion.i,
+                     mData.mQuaternion.j,
+                     mData.mQuaternion.k);
+        EulerAngles ea = q.ToEuler();
+        Orientation o;
+        o.SetEuler(ea.alpha,ea.beta,ea.gamma);
+        return o;
+    }
+    case EIGENSYSTEM: {
+        Orientation q=GetAsQuaternion();
+        return q.GetAsEuler();
+    }
+    case UNDEFINED:
+    default:
+        cerr << "Warning, cannot convert an undefined orientation" << endl;
+        return Orientation();
+    }
+}
+
+Orientation Orientation::GetAsEigenSystem() const {
+    switch(mType) {
+    case EULER: {
+        Orientation o = GetAsQuaternion();
+        return o.GetAsEigenSystem();
+    }
+    case ANGLE_AXIS: {
+        Orientation o = GetAsQuaternion();
+        return o.GetAsEigenSystem();
+    }
+    case QUATERNION: {
+        double W = mData.mQuaternion.real;
+        double X = mData.mQuaternion.i;
+        double Y = mData.mQuaternion.j;
+        double Z = mData.mQuaternion.k;
+
+        //Normalise the quaternion
+        double inv_mag = 1/(X*X + Y*Y + Z*Z + W*W);
+        X=X*inv_mag;
+        Y=Y*inv_mag;
+        Z=Z*inv_mag;
+        W=W*inv_mag;
+    
+        double xx = X * X;
+        double xy = X * Y;
+        double xz = X * Z;
+        double xw = X * W;
+
+        double yy = Y * Y;
+        double yz = Y * Z;
+        double yw = Y * W;
+
+        double zz = Z * Z;
+        double zw = Z * W;
+        
+        Orientation o;
+        o.SetEigenSystem(Vector3(1 - 2 * ( yy + zz ),2 * ( xy - zw ),2 * ( xz + yw )),
+                         Vector3(2 * ( xy + zw ),1 - 2 * ( xx + zz ),2 * ( yz - xw )),
+                         Vector3(2 * ( xz - yw ),2 * ( yz + xw ),1 - 2 * ( xx + yy )));
+        return o;
+    }
+    case EIGENSYSTEM:
+        return Orientation(*this);
+        break;
+    case UNDEFINED:
+    default:
+        cerr << "Warning, cannot convert an undefined orientation" << endl;
+            return Orientation();
+    }
+}
+
+Orientation Orientation::GetAsAngleAxis() const {
+    switch(mType) {
+    case EULER: {
+        Orientation o=GetAsQuaternion();
+        return o.GetAsAngleAxis();
+    }
+    case ANGLE_AXIS: {
+        return Orientation(*this);
+    }
+    case QUATERNION: {
+        double angle=2*acos(mData.mQuaternion.real);
+        double inv_sin_a=1/(sqrt(1-mData.mQuaternion.real*mData.mQuaternion.real)*2);
+        double vx=mData.mQuaternion.i*inv_sin_a;
+        double vy=mData.mQuaternion.j*inv_sin_a;
+        double vz=mData.mQuaternion.k*inv_sin_a;
+        Orientation o;
+        o.SetAngleAxis(angle,Vector3(vx,vy,vz));
+        return o;
+    }
+    case EIGENSYSTEM: {
+        Orientation o=GetAsQuaternion();
+        return o.GetAsAngleAxis();
+    }
+    case UNDEFINED:
+    default: {
+        cerr << "Warning, cannot convert an undefined orientation" << endl;
+        return Orientation();
+    }
+    }
+}
+
+Orientation Orientation::GetAsQuaternion() const {
+    switch(mType) {
+    case EULER: {
+	Quaternion q1(mData.mEuler.alpha,Vector3(0,0,1));
+	Quaternion q2(mData.mEuler.beta, Vector3(0,1,0));
+	Quaternion q3(mData.mEuler.gamma,Vector3(0,0,1));
+        Quaternion q=q3*q2*q1;
+        Orientation o;
+        o.SetQuaternion(q.w,q.x,q.y,q.z);
+        return o;
+    }
+    case ANGLE_AXIS: {
+	double sin_a=sin(mData.mAngleAxis.angle/2);
+	double w=cos(mData.mAngleAxis.angle/2);
+	double x=mData.mAngleAxis.axis->GetX()*sin_a;
+	double y=mData.mAngleAxis.axis->GetY()*sin_a;
+	double z=mData.mAngleAxis.axis->GetZ()*sin_a;
+        Orientation o;
+        o.SetQuaternion(w,x,y,z);
+        return o;
+    }
+    case QUATERNION: {
+        return Orientation(*this);
+    }
+    case EIGENSYSTEM: {
+        double W,X,Y,Z;
+        double m00=mData.mEigenSystem.XAxis->GetX();
+        double m01=mData.mEigenSystem.XAxis->GetY();
+        double m02=mData.mEigenSystem.XAxis->GetZ();
+
+        double m10=mData.mEigenSystem.YAxis->GetX();
+        double m11=mData.mEigenSystem.YAxis->GetY();
+        double m12=mData.mEigenSystem.YAxis->GetZ();
+
+        double m20=mData.mEigenSystem.ZAxis->GetX();
+        double m21=mData.mEigenSystem.ZAxis->GetY();
+        double m22=mData.mEigenSystem.ZAxis->GetZ();
+
+        double trace=m00+m11+m22 + 1;
+        if(trace > 0) {
+            double S = 0.5 / sqrt(trace);
+            W = 0.25 / S;
+            X = (m21 - m12)*S;
+            Y = (m02 - m20)*S;
+            Z = (m10 - m01)*S;
+        } else {
+            if(m00 > m11 && m00 > m22) {
+                double S  = sqrt(1.0 + m00 - m11 - m22)*2;
+                X = 0.5 / S;
+                Y = (m01 + m10)/S;
+                Z = (m02 + m21)/S;
+                W = (m12 + m21)/S;
+            } else if(m11 > m22) {
+                double S  = sqrt(1.0 + m11 - m22 - m00)*2;
+                X = (m01 + m10)/S;
+                Y = 0.5 / S;
+                Z = (m12 + m21)/S;
+                W = (m02 + m20)/S;
+            } else {
+                double S  = sqrt(1.0 + m22 - m11 - m00)*2;
+                X = (m02 + m20)/S;
+                Y = (m12 + m21)/S;
+                Z = 0.5 / S;
+                W = (m01 + m10)/S;
+            }
+        }
+        Orientation o;
+        o.SetQuaternion(W,X,Y,Z);
+        return o;
+    }
+    case UNDEFINED:
+    default: {
+        cerr << "Warning, cannot convert an undefined orientation" << endl;
+        return Orientation();
+    }
+    }
 }
 

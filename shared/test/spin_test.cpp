dcffa3652c827d2127191fab1c5ccb7161ba627e
Gareth Charnock <gareth.charnock@oerc.ox.ac.uk>
2010-10-11 13:37:06 +0100

    Some basic unit tests for the spin class

--- shared/test/spin_test.cpp
+++ shared/test/spin_test.cpp
@@ -2,9 +2,11 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
+#include <shared/test/common.hpp>
 #include <shared/spin.hpp>
 #include <iostream>
 
+using namespace std;
 using namespace SpinXML;
 
 struct Setup {
@@ -12,7 +14,9 @@ struct Setup {
         x(1.0 * Angstroms),
         y(2.0 * Angstroms),
         z(-3.0 * Angstroms),
-        s(new Spin(Vector3d(x.si,y.si,z.si),"Hello World",5)) {
+		label("Hello world"),
+		element(5),
+        s(new Spin(Vector3d(x.si,y.si,z.si),label,element)) {
     }
     ~Setup() {
         if(s != NULL) {
@@ -20,6 +24,8 @@ struct Setup {
         }
     }
     length x,y,z;
+	string label;
+	long element;
     Spin* s;
 };
 
@@ -30,7 +36,37 @@ BOOST_FIXTURE_TEST_CASE( SpinConstructor, Setup ) {
     BOOST_CHECK_EQUAL(tx.si,x.si);
     BOOST_CHECK_EQUAL(ty.si,y.si);
     BOOST_CHECK_EQUAL(tz.si,z.si);
+	BOOST_CHECK_EQUAL(s->GetElement(),element);
+	BOOST_CHECK_EQUAL(s->GetLabel(),label);
 }
 
 
+long signals_count = 0;
+void changeHandler() {
+	signals_count++;
+}
+Spin* deleted_spin=NULL;
+void dieHandler(Spin* spin) {
+	deleted_spin=spin;
+}
+BOOST_FIXTURE_TEST_CASE( Signals, Setup ) {
+	s->sigChange.connect(sigc::ptr_fun(&changeHandler));
+	s->sigDying.connect(sigc::ptr_fun(&dieHandler));
+
+	s->SetPosition(Vector3d(1,2,3));
+	BOOST_CHECK_EQUAL(signals_count,1);
+
+	s->SetCoordinates(1.0*Angstroms,2.0*Angstroms,3.0*Angstroms);
+	BOOST_CHECK_EQUAL(signals_count,2);
+
+	s->SetLabel("Changed label");
+	BOOST_CHECK_EQUAL(signals_count,3);
+
+	s->SetElement(1);
+	BOOST_CHECK_EQUAL(signals_count,4);
+
+	delete s;
+	BOOST_CHECK_EQUAL(deleted_spin,s);
+	s=NULL;
+}
 

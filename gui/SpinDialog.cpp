3280d087ad0dbf7b4cb73fb3d639ed104a213f9d
Gareth Charnock <gareth.charnock@oerc.ox.ac.uk>
2011-04-02 00:09:33 +0100

    Ajust the indentation to make consistent

--- gui/SpinDialog.cpp
+++ gui/SpinDialog.cpp
@@ -12,107 +12,107 @@
 using namespace SpinXML;
 
 enum {
-  ID_ELEMENT
+	ID_ELEMENT
 };
 
 SpinDialog::SpinDialog(wxWindow* parent,Spin* spin,wxWindowID id) 
 	: SpinDialogBase(parent,id),mSpin(spin) {
-  mInterEdit=new SpinInterEditPanel(mSpinDialogPanel);
-  mInterEdit->SetSpin(spin);
+	mInterEdit=new SpinInterEditPanel(mSpinDialogPanel);
+	mInterEdit->SetSpin(spin);
 
-  wxSizer* interSizer=mSpinDialogPanel->GetSizer()->GetItem(2)->GetSizer();
-  interSizer->Add(mInterEdit,1,wxEXPAND | wxALL);
+	wxSizer* interSizer=mSpinDialogPanel->GetSizer()->GetItem(2)->GetSizer();
+	interSizer->Add(mInterEdit,1,wxEXPAND | wxALL);
 
-  GetSizer()->Fit(this);
+	GetSizer()->Fit(this);
 
-  //Populate the element combo
+	//Populate the element combo
 
-  for(long i=0;i<getElementCount();i++) {
-    mElementCombo->Append(wxString() <<
-			  i <<
-			  wxT(" ") <<
-			  wxString(getElementSymbol(i),wxConvUTF8) <<
-			  wxT(" - ") <<
-			  wxString(getElementName(i),wxConvUTF8));
-  }
+	for(long i=0;i<getElementCount();i++) {
+		mElementCombo->Append(wxString() <<
+							  i <<
+							  wxT(" ") <<
+							  wxString(getElementSymbol(i),wxConvUTF8) <<
+							  wxT(" - ") <<
+							  wxString(getElementName(i),wxConvUTF8));
+	}
 
-  mElementCombo->SetId(ID_ELEMENT);
-  LoadFromSpin();
+	mElementCombo->SetId(ID_ELEMENT);
+	LoadFromSpin();
 }
 
 void SpinDialog::UpdateIsotopeDropDown() {
-  long p=mElementCombo->GetSelection();
+	long p=mElementCombo->GetSelection();
 
-  mIsotopeCombo->Clear();
-  for(long i=0;i<getIsotopeCount(p);i++) {
-    mIsotopeCombo->Append(wxString(getElementSymbol(p),wxConvUTF8) <<
-			  wxT("(") <<
-			  (getNeutrons(p,i) + p)<<
-			  wxT(")"));
-  }
-  if(getIsotopeCount(p)>0) {
-    mIsotopeCombo->SetSelection(0);
-  }
+	mIsotopeCombo->Clear();
+	for(long i=0;i<getIsotopeCount(p);i++) {
+		mIsotopeCombo->Append(wxString(getElementSymbol(p),wxConvUTF8) <<
+							  wxT("(") <<
+							  (getNeutrons(p,i) + p)<<
+							  wxT(")"));
+	}
+	if(getIsotopeCount(p)>0) {
+		mIsotopeCombo->SetSelection(0);
+	}
 }
  
 void SpinDialog::OnApply(wxCommandEvent& e) {
-  SaveToSpin();
+	SaveToSpin();
 }
 
 int SpinDialog::ShowModal() {
-  long result=wxDialog::ShowModal();
-  if(result==wxID_OK) {
-    SaveToSpin();
-  }
-  return result;
+	long result=wxDialog::ShowModal();
+	if(result==wxID_OK) {
+		SaveToSpin();
+	}
+	return result;
 }
 
 void SpinDialog::SaveToSpin() {
-  double x,y,z;
-  double parseError=false;
-  parseError = parseError || !mXPosCtrl->GetValue().ToDouble(&x);
-  parseError = parseError || !mYPosCtrl->GetValue().ToDouble(&y);
-  parseError = parseError || !mZPosCtrl->GetValue().ToDouble(&z);
-  //parseError=true if any of the above functions failed.
-  if(parseError) {
-    wxLogError(wxT("Failed to parse a floating point number"));
-    return;
-  }
+	double x,y,z;
+	double parseError=false;
+	parseError = parseError || !mXPosCtrl->GetValue().ToDouble(&x);
+	parseError = parseError || !mYPosCtrl->GetValue().ToDouble(&y);
+	parseError = parseError || !mZPosCtrl->GetValue().ToDouble(&z);
+	//parseError=true if any of the above functions failed.
+	if(parseError) {
+		wxLogError(wxT("Failed to parse a floating point number"));
+		return;
+	}
     
-  mSpin->SetCoordinates(x*Angstroms,y*Angstroms,z*Angstroms);
+	mSpin->SetCoordinates(x*Angstroms,y*Angstroms,z*Angstroms);
 
-  wxString label=mSpinTitle->GetValue();
-  mSpin->SetLabel(string(label.mb_str()));
+	wxString label=mSpinTitle->GetValue();
+	mSpin->SetLabel(string(label.mb_str()));
 
 
-  mSpin->SetElement(mElementCombo->GetSelection());
+	mSpin->SetElement(mElementCombo->GetSelection());
 }
 
 
 void SpinDialog::LoadFromSpin() {
-  length x,y,z;
-  mSpin->GetCoordinates(&x,&y,&z);
-  wxString label=wxString(mSpin->GetLabel(),wxConvUTF8);
+	length x,y,z;
+	mSpin->GetCoordinates(&x,&y,&z);
+	wxString label=wxString(mSpin->GetLabel(),wxConvUTF8);
 
-  mSpinTitle->SetValue(label);
+	mSpinTitle->SetValue(label);
 
-  wxString strX,strY,strZ;
-  strX << x * Angstroms;
-  strY << y * Angstroms;
-  strZ << z * Angstroms;
+	wxString strX,strY,strZ;
+	strX << x * Angstroms;
+	strY << y * Angstroms;
+	strZ << z * Angstroms;
 
-  mXPosCtrl->SetValue(strX);
-  mYPosCtrl->SetValue(strY);
-  mZPosCtrl->SetValue(strZ);
+	mXPosCtrl->SetValue(strX);
+	mYPosCtrl->SetValue(strY);
+	mZPosCtrl->SetValue(strZ);
 
-  mElementCombo->SetSelection(mSpin->GetElement());
+	mElementCombo->SetSelection(mSpin->GetElement());
 
-  UpdateIsotopeDropDown();
-  mIsotopeCombo->SetSelection(0);
+	UpdateIsotopeDropDown();
+	mIsotopeCombo->SetSelection(0);
 }
 
 void SpinDialog::OnElementChange(wxCommandEvent& e) {
-  UpdateIsotopeDropDown();
+	UpdateIsotopeDropDown();
 }
 
 
